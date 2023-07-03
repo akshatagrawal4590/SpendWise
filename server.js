12861ba81345aac70ae81a9e882ad7a63d203e4f
akshatagrawal4590 <akshatagrawal4590@gmail.com>
2023-07-04 00:35:40 +0530

    New commit

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const dbConnect = require("./dbConnect.js");
 const app = express();
 const port = process.env.port || 4000;
-app.use(express.json());
 const userRoute = require("./routes/userRoute.js");
 const transactionsRoute = require("./routes/transactionsRoute.js");
 const path = require("path");
+const mongoose = require("mongoose");
+app.use(express.json());
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect("mongodb+srv://admin-akshat:akshatmongo@cluster0.uued4pv.mongodb.net/Expense_TrackerApp", {useNewUrlParser: true, useUnifiedTopology: true});
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
 
 app.use("/api/users", userRoute);
 app.use("/api/transactions", transactionsRoute);
